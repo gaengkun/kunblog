e8b6c57f47eac3a4b319832f789edae1d0835a85
gaengkun <gaengkun@gmail.com>
2020-05-23 15:49:57 +0900

    깃 커밋

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-app.get('/', (req, res) => {
-  res.send(`Response Complate`);
-})
+app.get('/api/host', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send({ host : 'Kun'});
+});
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
-})
+});
 
